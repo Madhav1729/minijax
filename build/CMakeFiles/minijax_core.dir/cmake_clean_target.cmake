1ce995e92a8aa45294ebb4a54ed10a7ebc942b0d
madhav <mlnmadhavbhardwaj@gmail.com>
2026-09-02 20:28:44 +0530

    Ignore build artifacts

--- build/CMakeFiles/minijax_core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libminijax_core.a"
-)
--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mbhardwaj/Documents/madhav_files/placement_prep/minijax-cpp/minijax-cpp-archive/build/_deps/googletest-src
-# Build directory: /home/mbhardwaj/Documents/madhav_files/placement_prep/minijax-cpp/minijax-cpp-archive/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
